b503e1b5089d7cd7352f09e354a4b79f6931ddbe
Akos Hajba <hajbodev@gmail.com>
2022-04-25 00:26:55 +0200

    Working PoC

--- components/MainTable.tsx
+++ components/MainTable.tsx
@@ -1,20 +1,24 @@
-import { Button } from "rsuite";
+import { Button, DatePicker, InputPicker } from "rsuite";
 import { Table, Cell, Column, HeaderCell } from "rsuite-table";
-
 
 type AnyProps = any;
 
+const CURRENCIES = ["USD", "HUF", "EUR"];
+export const RATES = { USD: 350, HUF: 1, EUR: 400 };
+
 export const EditCell = (props: AnyProps) => {
-  const { rowData, dataKey, onChange } = props;
+  const { rowData, dataKey, handleChange } = props;
   const editing = rowData.status === "EDIT";
+  const padding = editing ? "6px" : "13px";
   return (
-    <Cell {...props} className={editing ? "table-content-editing" : ""}>
+    <Cell {...props} style={{ padding: padding }}>
       {editing ? (
         <input
           className="rs-input"
           defaultValue={rowData[dataKey]}
           onChange={(event) => {
-            onChange && onChange(rowData.id, dataKey, event.target.value);
+            handleChange &
+              handleChange(rowData.id, dataKey, event.target.value);
           }}
         />
       ) : (
@@ -24,19 +28,117 @@ export const EditCell = (props: AnyProps) => {
   );
 };
 
-const ActionCell = (props: AnyProps) => {
-  const { rowData, dataKey, onClick } = props;
+export const IncomeExpenseCell = (props: AnyProps) => {
+  const { rowData, dataKey, handleChange } = props;
+  let editing = rowData.status === "EDIT";
+  if (dataKey === "income" && rowData.expense) editing = false;
+  if (dataKey === "expense" && rowData.income) editing = false;
+
+  const padding = editing ? "6px" : "13px";
+  return (
+    <Cell {...props} style={{ padding: padding }}>
+      {editing ? (
+        <input
+          type="number"
+          className="rs-input"
+          defaultValue={rowData[dataKey]}
+          onChange={(event) => {
+            handleChange &
+              handleChange(rowData.id, dataKey, event.target.value);
+          }}
+        />
+      ) : (
+        <span className="table-content-edit-span">{rowData[dataKey]}</span>
+      )}
+    </Cell>
+  );
+};
+
+export const DateCell = (props: AnyProps) => {
+  const { rowData, dataKey, handleChange } = props;
+  const editing = rowData.status === "EDIT";
+  const padding = editing ? "6px" : "13px";
+  return (
+    <Cell {...props} style={{ padding: padding }}>
+      {editing ? (
+        <DatePicker
+          defaultValue={
+            rowData[dataKey] ? new Date(Date.parse(rowData[dataKey])) : null
+          }
+          onChange={(date) => {
+            handleChange &
+              handleChange(
+                rowData.id,
+                dataKey,
+                date?.toISOString().split("T")[0]
+              );
+          }}
+        />
+      ) : (
+        <span className="table-content-edit-span">{rowData[dataKey]}</span>
+      )}
+    </Cell>
+  );
+};
+
+export const DropdownCell = (props: AnyProps) => {
+  const { rowData, dataKey, handleChange, options } = props;
+  const editing = rowData.status === "EDIT";
+  const padding = editing ? "6px" : "13px";
+  return (
+    <Cell {...props} style={{ padding: padding }}>
+      {editing ? (
+        <InputPicker
+          data={options}
+          onChange={(value) => {
+            handleChange(rowData.id, dataKey, value);
+          }}
+        />
+      ) : (
+        <span className="table-content-edit-span">{rowData[dataKey]}</span>
+      )}
+    </Cell>
+  );
+};
+
+const EditButtonCell = (props: AnyProps) => {
+  const { rowData, dataKey, handleClick } = props;
 
   return (
     <Cell {...props} style={{ padding: "6px" }}>
       <Button
-        appearance="link"
         onClick={() => {
-          onClick && onClick(rowData.id);
+          handleClick & handleClick(rowData.id);
         }}
       >
-        {rowData.status === "EDIT" ? "Save" : "Edit"}
+        {rowData.status === "EDIT" ? "💾" : "⚙️"}
       </Button>
+    </Cell>
+  );
+};
+
+const DeleteRowButtonCell = (props: AnyProps) => {
+  const { rowData, dataKey, handleClick } = props;
+
+  return (
+    <Cell {...props} style={{ padding: "6px" }}>
+      <Button
+        onClick={() => {
+          handleClick & handleClick(rowData.id);
+        }}
+      >
+        ❌
+      </Button>
+    </Cell>
+  );
+};
+
+const SmallScaleCell = (props: AnyProps) => {
+  const { rowData, dataKey, handleClick } = props;
+
+  return (
+    <Cell {...props} style={{ padding: "13px" }}>
+      {rowData[dataKey] == null ? null : rowData[dataKey] ? "Igen" : "Nem"}
     </Cell>
   );
 };
@@ -52,68 +154,79 @@ export interface DataRow {
   comment?: string;
   smallScale?: boolean;
   status?: "EDIT" | null;
-};
-
+}
 
 type MainTableProps = {
   data: DataRow[];
   handleChange: (id: number, key: string, value: string) => void;
-  handleEditState: (id: number) => void;
-}
+  handleToggleState: (id: number) => void;
+  handleDeleteRow: (id: number) => void;
+};
 
 const MainTable = (props: MainTableProps) => {
-  const { data, handleChange, handleEditState } = props;
+  const { data, handleChange, handleToggleState, handleDeleteRow } = props;
 
   return (
-    <Table height={420} width={1200} data={data}>
+    <Table height={420} width={1400} data={data}>
       <Column width={80}>
         <HeaderCell>Sorszám</HeaderCell>
-        <EditCell dataKey="id" onChange={handleChange} />
+        <Cell dataKey="id" />
       </Column>
 
-      <Column width={150}>
+      <Column width={180}>
         <HeaderCell>Dátum (YYY.MM.DD.)</HeaderCell>
-        <EditCell dataKey="date" onChange={handleChange} />
+        <DateCell dataKey="date" handleChange={handleChange} />
       </Column>
 
       <Column width={100}>
         <HeaderCell>Bevétel</HeaderCell>
-        <EditCell dataKey="income" onChange={handleChange} />
+        <IncomeExpenseCell dataKey="income" handleChange={handleChange} />
       </Column>
 
       <Column width={100}>
         <HeaderCell>Kiadás</HeaderCell>
-        <EditCell dataKey="expense" onChange={handleChange} />
+        <IncomeExpenseCell dataKey="expense" handleChange={handleChange} />
       </Column>
 
-      <Column width={80}>
+      <Column width={120}>
         <HeaderCell>Pénznem</HeaderCell>
-        <EditCell dataKey="currency" onChange={handleChange} />
+        <DropdownCell
+          dataKey="currency"
+          handleChange={handleChange}
+          options={CURRENCIES.map((c) => {
+            return { value: c, label: c };
+          })}
+        />
       </Column>
 
       <Column width={120}>
         <HeaderCell>Árfolyam (HUF)</HeaderCell>
-        <EditCell dataKey="rate" onChange={handleChange} />
+        <Cell dataKey="rate" />
       </Column>
 
       <Column width={120}>
         <HeaderCell>Eredmény (HUF)</HeaderCell>
-        <EditCell dataKey="result" onChange={handleChange} />
+        <Cell dataKey="result" />
       </Column>
 
       <Column width={200}>
         <HeaderCell>Megjegyzés</HeaderCell>
-        <EditCell dataKey="comment" onChange={handleChange} />
+        <EditCell dataKey="comment" handleChange={handleChange} />
       </Column>
 
       <Column width={130}>
         <HeaderCell>Kisértékű ügylet?</HeaderCell>
-        <EditCell dataKey="smallScale" onChange={handleChange} />
+        <SmallScaleCell dataKey="smallScale" />
       </Column>
 
       <Column flexGrow={1}>
-        <HeaderCell>Action</HeaderCell>
-        <ActionCell dataKey="id" onClick={handleEditState} />
+        <HeaderCell>{null}</HeaderCell>
+        <EditButtonCell dataKey="edit" handleClick={handleToggleState} />
+      </Column>
+
+      <Column flexGrow={1}>
+        <HeaderCell>{null}</HeaderCell>
+        <DeleteRowButtonCell dataKey="delete" handleClick={handleDeleteRow} />
       </Column>
     </Table>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,78 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 import { Button, List } from "rsuite";
-import MainTable, { DataRow } from "../components/MainTable";
+import MainTable, { DataRow, RATES } from "../components/MainTable";
 import styles from "../styles/Home.module.css";
 
 const MinimumIncome = 167400;
 const MinimumIncome10pc = MinimumIncome * 0.1;
 const TaxKey = 15;
 
+interface TaxResults {
+  currentYearLoss: number;
+  currentYearIncome: number;
+  currentYearSmallScaleIncome: number;
+  currentYearTaxableIncome: number;
+  currentYearTaxableLoss: number;
+  currentYearTax: number;
+}
+
 const Home: NextPage = () => {
   const [data, setData] = useState<DataRow[]>([]);
+  const [taxResults, setTaxResults] = useState<TaxResults | null>(null);
 
   const handleChange = (id: number, key: string, value: any) => {
     const nextData = [...data];
     const editedItem = nextData.find((item) => item.id === id);
     if (editedItem) {
       editedItem[key as keyof DataRow] = value;
+
+      if (editedItem.currency)
+        editedItem.rate = RATES[editedItem.currency as keyof typeof RATES];
+      if (editedItem.rate) {
+        if (editedItem.income) {
+          editedItem.result = editedItem.income * editedItem.rate;
+          editedItem.smallScale = editedItem.result <= MinimumIncome10pc;
+        } else if (editedItem.expense) {
+          editedItem.result = -1 * editedItem.expense * editedItem.rate;
+          editedItem.smallScale = false; // TODO: Not sure about it
+        }
+      }
     }
+
+    const sumResult = nextData.reduce(
+      (sum, item) => sum + (item.result || 0),
+      0
+    );
+    const sumSmallScale = nextData.reduce(
+      (sum, item) => sum + (item.smallScale ? item.result || 0 : 0),
+      0
+    );
+
+    const currentYearLoss = sumResult < 0 ? -1 * sumResult : 0;
+    const currentYearIncome = sumResult > 0 ? sumResult : 0;
+
+    const taxableIncome =
+      sumResult - sumSmallScale > 0 ? sumResult - sumSmallScale : 0;
+    const taxableLoss =
+      currentYearLoss - sumSmallScale !== -1 * sumSmallScale
+        ? Math.abs(currentYearLoss - sumSmallScale)
+        : 0;
+
+    const newTaxResults = {
+      currentYearLoss: currentYearLoss,
+      currentYearIncome: currentYearIncome,
+      currentYearSmallScaleIncome: sumSmallScale,
+      currentYearTaxableIncome: taxableIncome,
+      currentYearTaxableLoss: taxableLoss,
+      currentYearTax: taxableIncome * (TaxKey / 100),
+    };
+
+    setTaxResults(newTaxResults);
     setData(nextData);
   };
-  const handleEditState = (id: number) => {
+
+  const handleToggleState = (id: number) => {
     const nextData = [...data];
     const activeItem = nextData.find((item) => item.id === id);
     if (activeItem) {
@@ -29,10 +82,15 @@ const Home: NextPage = () => {
     setData(nextData);
   };
 
+  const handleDeleteRow = (id: number) => {
+    const nextData = data.filter((item) => item.id !== id);
+    setData(nextData);
+  };
+
   const newRow = () => {
     const nextData = [...data, { id: data.length + 1 }];
     setData(nextData);
-  }
+  };
 
   return (
     <div className={styles.container}>
@@ -46,12 +104,13 @@ const Home: NextPage = () => {
         <MainTable
           data={data}
           handleChange={handleChange}
-          handleEditState={handleEditState}
+          handleToggleState={handleToggleState}
+          handleDeleteRow={handleDeleteRow}
         />
         <Button
           appearance="default"
           block
-          style={{ height: 24, width: 1200, padding: 2 }}
+          style={{ height: 24, width: 1400, padding: 2 }}
           onClick={newRow}
         >
           + Új sor
@@ -70,24 +129,30 @@ const Home: NextPage = () => {
             <List.Item>
               <b>TOTAL tárgyév eredmény (HUF)</b>
             </List.Item>
-            <List.Item>Veszteség: </List.Item>
-            <List.Item>Jövedelem: </List.Item>
+            <List.Item>Veszteség: {taxResults?.currentYearLoss}</List.Item>
+            <List.Item>Jövedelem: {taxResults?.currentYearIncome}</List.Item>
           </List>
 
           <List>
             <List.Item>
               <b>TOTAL kisértékű bevételek (HUF)</b>
             </List.Item>
-            <List.Item>Jövedelem: </List.Item>
+            <List.Item>
+              Jövedelem: {taxResults?.currentYearSmallScaleIncome}
+            </List.Item>
           </List>
 
           <List>
             <List.Item>
               <b>TOTAL tárgyév adózandó eredmény (HUF)</b>
             </List.Item>
-            <List.Item>Veszteség: </List.Item>
-            <List.Item>Jövedelem: </List.Item>
-            <List.Item>Adó: </List.Item>
+            <List.Item>
+              Veszteség: {taxResults?.currentYearTaxableLoss}
+            </List.Item>
+            <List.Item>
+              Jövedelem: {taxResults?.currentYearTaxableIncome}
+            </List.Item>
+            <List.Item>Adó: {taxResults?.currentYearTax}</List.Item>
           </List>
 
           <span>SZJA bevallás 164. sor</span>
